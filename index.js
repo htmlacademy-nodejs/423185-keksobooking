e59bbc4c0d76a45e4c1889f7d3643783b136ec4e
Anton Lantukh <lantukh@bk.ru>
2018-09-30 23:51:58 +0800

    Corrections

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-const projectName = "keksobooking";
-const currentVersion = "v0.0.1";
-const author = "Anton Lantukh";
-const defaultOutput = `Привет пользователь!\n
-Эта программа будет запускать сервер «${projectName}».\n
-Автор: ${author}.;\n`;
-const helpOption = `Доступные команды:\n
---help    — печатает этот текст;\n
---version — печатает версию приложения;`;
-
-const showError = command =>
-  `Неизвестная команда ${command}.\n Чтобы прочитать правила использования приложения, наберите "--help"`;
-const renderInput = key => {
-  switch (key) {
-    case "--version":
-      console.log(currentVersion);
-      break;
-    case "--help":
-      console.log(helpOption);
-      break;
-    default:
-      console.error(showError(key));
-  }
+const PROJECT = {
+  NAME: "keksobooking",
+  VERSION: "v0.0.1",
+  AUTHOR: "Anton Lantukh"
 };
 
-process.stdout.write(defaultOutput);
-process.stdin.on("data", data => {
-  renderInput(data.toString().trim());
-});
+const OUTPUT = {
+  DEFAULT: `Привет пользователь!\n
+    Эта программа будет запускать сервер «${PROJECT.NAME}».\n
+    Автор: ${PROJECT.AUTHOR}.;\n`,
+  HELP: `Доступные команды:\n
+    --help    — печатает этот текст;\n
+    --version — печатает версию приложения;`
+};
+
+const args = process.argv.slice(2);
+const showError = command =>
+  `Неизвестная команда ${command}.\n Чтобы прочитать правила использования приложения, наберите "--help"`;
+
+switch (args[0]) {
+  case "--version":
+    console.log(PROJECT.VERSION);
+    break;
+  case "--help":
+    console.log(OUTPUT.HELP);
+    break;
+  case undefined:
+    console.log(OUTPUT.DEFAULT);
+    break;
+  default:
+    console.error(showError(args[0]));
+}
