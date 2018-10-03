19dfd31456950101ba1e120e16aad3685c17a34d
Anton Lantukh <lantukh@bk.ru>
2018-10-03 15:23:32 +0400

    Small remarks

--- index.js
+++ index.js
@@ -15,17 +15,15 @@ const arg = process.argv.slice(2);
 const value = arg[0];
 
 if (value) {
-  const command = commands.find((item) => {
-    return value.substr(2) === item.name;
-  });
+  const command = commands.find((item) => value === `--${item.name}`);
   if (command) {
     command.execute(commands);
-    process.exit(0);
   } else {
     error.execute(value);
     process.exit(1);
   }
 } else {
   defaultMessage.execute(commands);
-  process.exit(0);
 }
+
+process.exit(0);
