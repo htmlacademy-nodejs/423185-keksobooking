79a6ec1af847edc1c225a0f3e47b88538aecfc90
Anton Lantukh <lantukh@bk.ru>
2018-11-05 22:03:17 +0300

    Minor fix

--- src/data/util.js
+++ src/data/util.js
@@ -37,7 +37,7 @@ const shuffle = (array) => {
 };
 
 const timestampToDate = (ts) => {
-  const d = +new Date();
+  const d = new Date();
   if (d.setTime(ts)) {
     return (`0` + d.getDate()).slice(-2) + `.` + (`0` + (d.getMonth() + 1)).slice(-2) + `.` + d.getFullYear();
   } else {
